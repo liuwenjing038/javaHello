3075e4ec3945f4f0f49c00c46a228190a7b76c4e
liuwenjing038 <liuwenjing038@ke.com>
2024-05-17 15:18:52 +0800

    提交一下冲突生成

--- howTo/src/testMain.java
+++ howTo/src/testMain.java
@@ -2,5 +2,6 @@ public class testMain {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("idea 打印again！");
+        System.out.println("制造个冲突");
     }
 }
