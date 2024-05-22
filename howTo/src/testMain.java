a23393425cd756bdb36ffd5557cc416dbc7f72ea
liuwenjing038 <liuwenjing038@ke.com>
2024-05-22 10:08:57 +0800

    first commit

--- howTo/src/testMain.java
+++ howTo/src/testMain.java
@@ -1,7 +1,7 @@
 public class testMain {
     public static void main(String[] args) {
         System.out.println("hello world!");
-
+        System.out.println("hello world again!");
         System.out.println("idea 打印again！!");
     }
 }
