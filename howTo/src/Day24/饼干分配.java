13752842945d3818b1b4712a4b673f1cea72a81f
liuwenjing038 <liuwenjing038@ke.com>
2024-06-26 11:16:12 +0800

    到贪心了

---
+++ howTo/src/Day24/饼干分配.java
@@ -0,0 +1,16 @@
+package Day24;
+
+public class 饼干分配 {
+    public int findContentChildren(int[] g, int[] s) {
+        int result = 0;
+        int count = 0;
+        //这里需要确认胃口数组不越界
+        for (int i = 0; i < s.length && count < g.length; i++) {
+            if (s[i] >= g[count]){
+                result++;
+                count++;
+            }
+        }
+        return result;
+    }
+}
